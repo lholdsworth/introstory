b45290b3840b29b1aa819fe4c5c7ec2411144293
lholdsworth <iamholdsworth@gmail.com>
2017-04-20 19:13:36 +0100

    work please

--- intro/settings.py
+++ intro/settings.py
@@ -173,8 +173,8 @@ except ImportError:
 #     from aws_settings import *
 
 AWS_STORAGE_BUCKET_NAME = 'intro-story'
-AWS_ACCESS_KEY_ID = 'AKIAIPTAG6WRYSV3FVTQ'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
